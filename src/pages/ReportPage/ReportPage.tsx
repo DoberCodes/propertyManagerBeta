c83d10430b6c35df18c67ed92ff38343de17faf5
Austin <austin.dober@gmail.com>
2026-02-19 00:32:52 -0500

    fix: fixing report page

--- src/pages/ReportPage/ReportPage.tsx
+++ src/pages/ReportPage/ReportPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ReportBuilder } from '../../Components/ReportBuilder';
+import { ReportBuilder } from 'Components/ReportBuilder/ReportBuilder';
 
 export const ReportPage = () => {
 	return <ReportBuilder />;
